2d5d5908416c5e6be5a74eb7a8c38a60cbe19eca
Zubair <hashmatzubair32@gmail.com>
2026-06-03 09:19:23 +0500

    variables

--- variables.js
+++ variables.js
@@ -1,5 +1,5 @@
 // Variables
-const accountID = 14444  // Values assigned to a constant variable cannot be changed
+const accountID = 14444  // Values assigned to a constant variable cannot be changed.
 let accountname = "John Doe"
 var accountpassword = "password123" // values assigned to a var variable can be changed but var is function-scoped and can lead to issues in larger codebases, so it's generally recommended to use let or const instead.
 accontcity = "New York"
