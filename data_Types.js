dfc60396010f98d3d948dd4a11160b4201b01458
Zubair <hashmatzubair32@gmail.com>
2026-06-03 12:07:37 +0500

    Data Types

---
+++ data_Types.js
@@ -0,0 +1,21 @@
+console.log(typeof "Hello World"); // string
+console.log(typeof null); // null
+console.log(typeof undefined); // undefined
+// Data types in JavaScript
+// 1. Primitive Data Types
+let name = "John"; // string
+let age = 30; // number
+let isStudent = true; // boolean
+let nullValue = null; // null
+let undefinedValue; // undefined
+console.log(name); // John
+console.log(age); // 30
+console.log(isStudent); // true
+console.log(nullValue); // null
+console.log(undefinedValue); // undefined
+// Symbol data type
+let symbol1 = Symbol("symbol1");
+let symbol2 = Symbol("symbol2");
+console.log(symbol1); // Symbol(symbol1)
+console.log(symbol2); // Symbol(symbol2)
+console.log(typeof symbol1); // symbol
