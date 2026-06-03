56a8b035f4329950ea206938cd70c2429cfccf1e
Zubair <hashmatzubair32@gmail.com>
2026-06-03 15:27:31 +0500

    Conversion_Operation

---
+++ Conversion_Operation.js
@@ -0,0 +1,35 @@
+/*let score = "100"
+console.log(typeof score) // 100
+// Conversion of Data Types
+let valueInNumber = Number(score) // String to Number
+console.log(typeof valueInNumber) // number
+console.log(valueInNumber) // 100
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn) // Number to Boolean
+console.log(typeof booleanIsLoggedIn) // boolean
+console.log(booleanIsLoggedIn) // true
+let someNumber = 100
+let stringNumber = String(someNumber) // Number to String
+console.log(typeof stringNumber) // string
+console.log(stringNumber) // "100" */
+// Operations in JavaScript
+let value = 3
+let negativeValue = -value // Unary Negation
+console.log(negativeValue) // -3
+// Increment and Decrement
+let postIncrement = value++ // Post Increment
+console.log(postIncrement) // 3
+console.log(value) // 4
+let preIncrement = ++value // Pre Increment
+console.log(preIncrement) // 5
+console.log(value) // 5
+// String Concatenation
+let string1 = "Hello"
+let string2 = "World"   
+let concatenatedString = string1 + " " + string2 // String Concatenation
+console.log(concatenatedString) // "Hello World"
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log("1" + 2 + 3) // "123"
+// console.log(1 + 2 + "3") // "33"
