d02c58b7125831395faafeaede5980990d05b7b1
Zubair <hashmatzubair32@gmail.com>
2026-06-06 21:56:48 +0500

    Add example of reduce method for array and object summation in nine.js

---
+++ Iterations/nine.js
@@ -0,0 +1,35 @@
+// Reduce method takes an entire array and distills it down into a single final value (like a total sum, 
+// a multiplied product, or even a single combined object).
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
