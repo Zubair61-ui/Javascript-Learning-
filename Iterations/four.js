37a4ff0d5ec7fe3ada3e35c373816d27096eb7e5
Zubair <hashmatzubair32@gmail.com>
2026-06-06 18:21:06 +0500

    Add examples demonstrating the use of the for-in loop for objects and arrays

---
+++ Iterations/four.js
@@ -0,0 +1,28 @@
+// for in loop   
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'C++',
+//     rb: "ruby",
+//     swift: "swift by apple"
+// }
+// // We can iterate through objects with the help of `for in` loop
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+// // We can also iterate through arrays,objects specially keys with the help of `for in` in loop
+// const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
+
+// Maps are not iterable so, we cannot use them with `for in` loop
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
